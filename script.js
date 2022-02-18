28553ed3ba1bc2b854199df453355020ccc726c8
Matt11711 <matthew.d.allen97@gmail.com>
2022-02-18 13:29:03 -0500

    updated comments for first couple functions and fixed some small bugs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Assignment Code
+// selects where to read button click from
 var generateBtn = document.querySelector("#generate");
-// arrays for each possible type of character in the password, to be added to the password array based on user input
+
+// arrays for each possible type of character in the password
 var lowercaseArray = [
   "q",
   "w",
@@ -29,35 +30,9 @@ var lowercaseArray = [
   "n",
   "m",
 ];
-var uppercaseArray = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  "Z",
-  "X",
-  "C",
-  "V",
-  "B",
-  "N",
-  "M",
-];
+
 var numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
 var symbolArray = [
   " ",
   "!",
@@ -93,22 +68,30 @@ var symbolArray = [
   "}",
   "~",
 ];
-finalPassword = ""
-var randomNumberForArray = function (arrayLength) {
-  var value = Math.floor(Math.random() * arrayLength);
 
-  return value;
+// function to return uppercase value of an input letter
+var toUpper = function (letter) {
+  return letter.toUpperCase();
 };
+
+// calls toUpper for each element of lowercaseArray, making the uppercaseArray
+var uppercaseArray = lowercaseArray.map(toUpper);
+
+// returns random number between 0 and the length of an array
+var randomNumberForArray = function (arrayLength) {
+  return Math.floor(Math.random() * arrayLength);
+};
+
 // Write password to the #password input
 function writePassword() {
- 
-  var password = finalPassword
- 
+  //selects where to put password
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  // writes password to that location
+  passwordText.value = finalPassword;
 }
+// function to generate password length
 generatePasswordLength = function () {
+  // resetting some variables in case the button is pressed again. Maybe should declare these all as global at the top of the script but I don't know of any reason to yet
   lowercaseCharacterCheck = false;
   uppercaseCharacterCheck = false;
   numCharacterCheck = false;
@@ -117,29 +100,29 @@ generatePasswordLength = function () {
   uppercaseCharacterRequired = false;
   numCharacterRequired = false;
   symbolCharacterRequired = false;
-  finalPassword = ""
+  finalPassword = "";
+  // lets user input password length
   var passwordLength = prompt(
     "How long would you like your new password to be?",
     8
   );
+  // turns the password length into a num for comparisons, and also rounds and decimals that were input by the user
   passwordLengthNum = parseInt(passwordLength);
+  // checks for password length fitting the parameters and brings them back to the prompt for length if their password length wasn't valid. If it is valid, moves on to generating the password
   if (
-    !passwordLength ||
-    passwordLength < 4 ||
-    passwordLength > 128 ||
+    !passwordLengthNum ||
+    passwordLengthNum < 8 ||
+    passwordLengthNum > 128 ||
     isNaN(passwordLength)
   ) {
     alert("Please choose a numerical length between 8 and 128.");
     generatePasswordLength();
-  }
-  else
-  generatePasswordCriteria()
+  } else generatePasswordCriteria();
 };
 
- 
 
-var generatePasswordCriteria = function() {
-  passwordArray = []; 
+var generatePasswordCriteria = function () {
+  passwordArray = [];
 
   var lowercaseConfirm = confirm(
     "Do you want your password to include lowercase letters?"
@@ -174,80 +157,73 @@ var generatePasswordCriteria = function() {
     alert("Please choose at least one type of character for your password");
     generatePasswordCriteria();
     return;
-  }
-else generatePassword()
-}
+  } else generatePassword();
+};
 
-
-  var generatePassword = function () {
-    lowercaseCharacterCheck = false;
+var generatePassword = function () {
+  lowercaseCharacterCheck = false;
   uppercaseCharacterCheck = false;
   numCharacterCheck = false;
   symbolCharacterCheck = false;
-  var password = ""
+  var password = "";
   for (var i = 1; i <= passwordLengthNum; i++) {
-    
-      var nextCharacterLocation = randomNumberForArray(passwordArray.length);
-     
-      if (lowercaseCharacterRequired && !lowercaseCharacterCheck) {
-        for (
-          var lowercaseIndex = 0;
-          lowercaseIndex < lowercaseArray.length;
-          lowercaseIndex++
-        ) {
-          if (
-            lowercaseArray[lowercaseIndex] ===
-            passwordArray[nextCharacterLocation]
-          ) {
-            lowercaseCharacterCheck = true;
-            break;
-          }
-        }
-      }
-      if (uppercaseCharacterRequired && !uppercaseCharacterCheck) {
-        for (
-          var uppercaseIndex = 0;
-          uppercaseIndex < uppercaseArray.length;
-          uppercaseIndex++
-        ) {
-          if (
-            uppercaseArray[uppercaseIndex] ===
-            passwordArray[nextCharacterLocation]
-          ) {
-            uppercaseCharacterCheck = true;
-            break;
-          }
-        }
-      }
-      if (numCharacterRequired && !numCharacterCheck) {
-        for (var numIndex = 0; numIndex < numArray.length; numIndex++) {
-          if (numArray[numIndex] === passwordArray[nextCharacterLocation]) {
-            numCharacterCheck = true;
-            break;
-          }
-        }
-      }
+    var nextCharacterLocation = randomNumberForArray(passwordArray.length);
 
-      if (symbolCharacterRequired && !symbolCharacterCheck) {
-        for (
-          var symbolIndex = 0;
-          symbolIndex < symbolArray.length;
-          symbolIndex++
+    if (lowercaseCharacterRequired && !lowercaseCharacterCheck) {
+      for (
+        var lowercaseIndex = 0;
+        lowercaseIndex < lowercaseArray.length;
+        lowercaseIndex++
+      ) {
+        if (
+          lowercaseArray[lowercaseIndex] ===
+          passwordArray[nextCharacterLocation]
         ) {
-          if (
-            symbolArray[symbolIndex] === passwordArray[nextCharacterLocation]
-          ) {
-            symbolCharacterCheck = true;
-            break;
-          }
+          lowercaseCharacterCheck = true;
+          break;
         }
       }
+    }
+    if (uppercaseCharacterRequired && !uppercaseCharacterCheck) {
+      for (
+        var uppercaseIndex = 0;
+        uppercaseIndex < uppercaseArray.length;
+        uppercaseIndex++
+      ) {
+        if (
+          uppercaseArray[uppercaseIndex] ===
+          passwordArray[nextCharacterLocation]
+        ) {
+          uppercaseCharacterCheck = true;
+          break;
+        }
+      }
+    }
+    if (numCharacterRequired && !numCharacterCheck) {
+      for (var numIndex = 0; numIndex < numArray.length; numIndex++) {
+        if (numArray[numIndex] === passwordArray[nextCharacterLocation]) {
+          numCharacterCheck = true;
+          break;
+        }
+      }
+    }
 
-      password = password + passwordArray[nextCharacterLocation];
-   
-    
+    if (symbolCharacterRequired && !symbolCharacterCheck) {
+      for (
+        var symbolIndex = 0;
+        symbolIndex < symbolArray.length;
+        symbolIndex++
+      ) {
+        if (symbolArray[symbolIndex] === passwordArray[nextCharacterLocation]) {
+          symbolCharacterCheck = true;
+          break;
+        }
+      }
+    }
+
+    password = password + passwordArray[nextCharacterLocation];
   }
- 
+
   if (
     lowercaseCharacterCheck != lowercaseCharacterRequired ||
     uppercaseCharacterCheck != uppercaseCharacterRequired ||
@@ -255,155 +231,13 @@ else generatePassword()
     symbolCharacterCheck != symbolCharacterRequired
   ) {
     generatePassword();
-  }
-  else
-  finalPassword = password
-  writePassword()
-
+  } else finalPassword = password;
+  writePassword();
 };
 
 var concatPasswordArray = function (arrayToConcat) {
   passwordArray = passwordArray.concat(arrayToConcat);
 };
-var tryAgain = function () {
-  lowercaseCharacterCheck = false;
-  uppercaseCharacterCheck = false;
-  numCharacterCheck = false;
-  symbolCharacterCheck = false;
-  console.log("HI")
-  for (var i = 1; i <= passwordLengthNum; i++) {
-    if (password) {
-      var nextCharacterLocation = randomNumberForArray(passwordArray.length);
-     
-      if (lowercaseCharacterRequired && !lowercaseCharacterCheck) {
-        for (
-          var lowercaseIndex = 0;
-          lowercaseIndex < lowercaseArray.length;
-          lowercaseIndex++
-        ) {
-          if (
-            lowercaseArray[lowercaseIndex] ===
-            passwordArray[nextCharacterLocation]
-          ) {
-            lowercaseCharacterCheck = true;
-            break;
-          }
-        }
-      }
-      if (uppercaseCharacterRequired && !uppercaseCharacterCheck) {
-        for (
-          var uppercaseIndex = 0;
-          uppercaseIndex < uppercaseArray.length;
-          uppercaseIndex++
-        ) {
-          if (
-            uppercaseArray[uppercaseIndex] ===
-            passwordArray[nextCharacterLocation]
-          ) {
-            uppercaseCharacterCheck = true;
-            break;
-          }
-        }
-      }
-      if (numCharacterRequired && !numCharacterCheck) {
-        for (var numIndex = 0; numIndex < numArray.length; numIndex++) {
-          if (numArray[numIndex] === passwordArray[nextCharacterLocation]) {
-            numCharacterCheck = true;
-            break;
-          }
-        }
-      }
 
-      if (symbolCharacterRequired && !symbolCharacterCheck) {
-        for (
-          var symbolIndex = 0;
-          symbolIndex < symbolArray.length;
-          symbolIndex++
-        ) {
-          if (
-            symbolArray[symbolIndex] === passwordArray[nextCharacterLocation]
-          ) {
-            symbolCharacterCheck = true;
-            break;
-          }
-        }
-      }
-
-      password = password + passwordArray[nextCharacterLocation];
-    } else {
-      var nextCharacterLocation = randomNumberForArray(passwordArray.length);
-
-      
-        if (lowercaseCharacterRequired && !lowercaseCharacterCheck) {
-          for (
-            var lowercaseIndex = 0;
-            lowercaseIndex < lowercaseArray.length;
-            lowercaseIndex++
-          ) {
-            if (
-              lowercaseArray[lowercaseIndex] ===
-              passwordArray[nextCharacterLocation]
-            ) {
-              lowercaseCharacterCheck = true;
-              break;
-            }
-          }
-        }
-        if (uppercaseCharacterRequired && !uppercaseCharacterCheck) {
-          for (
-            var uppercaseIndex = 0;
-            uppercaseIndex < uppercaseArray.length;
-            uppercaseIndex++
-          ) {
-            if (
-              uppercaseArray[uppercaseIndex] ===
-              passwordArray[nextCharacterLocation]
-            ) {
-              uppercaseCharacterCheck = true;
-              break;
-            }
-          }
-        }
-        if (numCharacterRequired && !numCharacterCheck) {
-          for (var numIndex = 0; numIndex < numArray.length; numIndex++) {
-            if (numArray[numIndex] === passwordArray[nextCharacterLocation]) {
-              numCharacterCheck = true;
-              break;
-            }
-          }
-        }
-
-        if (symbolCharacterRequired && !symbolCharacterCheck) {
-          for (
-            var symbolIndex = 0;
-            symbolIndex < symbolArray.length;
-            symbolIndex++
-          ) {
-            if (
-              symbolArray[symbolIndex] === passwordArray[nextCharacterLocation]
-            ) {
-              symbolCharacterCheck = true;
-              break;
-            }
-          }
-        }
-
-        var password = passwordArray[nextCharacterLocation];
-      }
-    
-  }
- 
-  if (
-    lowercaseCharacterCheck != lowercaseCharacterRequired ||
-    uppercaseCharacterCheck != uppercaseCharacterRequired ||
-    numCharacterCheck != numCharacterRequired ||
-    symbolCharacterCheck != symbolCharacterRequired
-  ) {
-    tryAgain();
-  }
-  else
-  finalPassword = password;
-  writePassword()
-};
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePasswordLength);
